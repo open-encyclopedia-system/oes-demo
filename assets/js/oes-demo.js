b5d8fe708888a4e2a98e39c21102e482fad123ad
Open Encyclopedia System <61697206+open-encyclopedia-system@users.noreply.github.com>
2023-04-28 11:35:24 +0200

    Fix missing folder

---
+++ assets/js/oes-demo.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function () {
+
+    /* Initialize map ------------------------------------------------------------------------------------------------*/
+    jQuery(".oes-resize-columns-6-6").on("click", function (event) {
+        oesMap.map.invalidateSize();
+    });
+});
