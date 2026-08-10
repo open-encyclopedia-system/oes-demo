dc79da771ffd1a03db3234f65c193b19f51ab297
Maren Strobl <maren.strobl@fu-berlin.de>
2026-08-10 20:52:09 +0200

    add missing file

---
+++ includes/theme/taxonomies/class-demo_term.php
@@ -0,0 +1,13 @@
+<?php
+
+if (!defined('ABSPATH')) exit; // Exit if accessed directly
+
+/**
+ * Class Demo_Term
+ */
+if (class_exists('OES_Term')) :
+
+    class Demo_Term extends OES_Term
+    {
+    }
+endif;
